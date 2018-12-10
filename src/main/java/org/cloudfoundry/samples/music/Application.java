6c73fdca1d2396da3da1c7c6c43d5350f83b3a73
Hermann Schnyder (tzhsche7) <Hermann.Schnyder@swisscom.com>
2018-12-10 07:28:01 +0100

    simplified and stripped down to JPA

--- src/main/java/org/cloudfoundry/samples/music/Application.java
+++ src/main/java/org/cloudfoundry/samples/music/Application.java
@@ -1,6 +1,5 @@
 package org.cloudfoundry.samples.music;
 
-import org.cloudfoundry.samples.music.config.SpringApplicationContextInitializer;
 import org.cloudfoundry.samples.music.repositories.AlbumRepositoryPopulator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,7 +9,6 @@ public class Application {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
-                .initializers(new SpringApplicationContextInitializer())
                 .listeners(new AlbumRepositoryPopulator())
                 .application()
                 .run(args);
